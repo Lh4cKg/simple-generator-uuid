53a0d19f1ca54d551f9aaf80ffd7068dbe6c247e
Lasha Gogua <lhackg@yandex.com>
2016-01-25 12:59:14 +0400

    update example

--- example.py
+++ example.py
@@ -7,6 +7,8 @@
 >>> uuid = _id.uuid()
 >>> print("return uuid is '%s'." % uuid)
 return uuid is 'p9KD3aSMEc8KAG2XCsoTA2'.
+>>> print("Author is %s," % __author__, "Package Version is %s" % __version__)
+Author is Lasha Gogua, Version is 0.1.0
 """
 
 import genuuid as _id
@@ -15,4 +17,4 @@ from genuuid import __author__,__version__
 uuid = _id.uuid()
 print("return uuid is '%s'." % uuid)
 
-print("Author is %s" % __author__, "Package Version is %s" %__version__)
+print("Author is %s," % __author__, "Package Version is %s" % __version__)
