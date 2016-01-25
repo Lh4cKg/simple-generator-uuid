591b15a273289898268f320d4cc3a4aff2e71e56
Lasha Gogua <lhackg@yandex.com>
2016-01-25 12:23:05 +0400

    add example.py

---
+++ example.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+"""
+>>> import genuuid as _id
+>>> uuid = _id.uuid()
+>>> print("return uuid is '%s'." % uuid)
+return uuid is 'p9KD3aSMEc8KAG2XCsoTA2'.
+"""
+
+import genuuid as _id
+
+uuid = _id.uuid()
+print("return uuid is '%s'." % uuid)
