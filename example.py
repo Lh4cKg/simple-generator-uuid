5361d3656ff925f476d3238ec66e8492e0f0308f
Lasha Gogua <lhackg@yandex.com>
2016-01-29 18:50:32 +0400

    update example.py

--- example.py
+++ example.py
@@ -4,9 +4,12 @@
 
 """
 >>> import genuuid as _id
->>> uuid = _id.uuid()
->>> print("return uuid is '%s'." % uuid)
-return uuid is 'p9KD3aSMEc8KAG2XCsoTA2'.
+>>> uuid = _id.luuid()
+>>> uuid1 = _id.suuid()
+>>> print("return uuid is '%s'." % uuid, "uuid length is %s" % len(uuid))
+return uuid is 'p9KD3aSMEc8KAG2XCsoTA2', uuid length is 22
+>>> print("return uuid1 is '%s'." % uuid)
+return uuid is 'KL3raivgvb5', uuid length is 11
 >>> print("Author is %s," % __author__, "Package Version is %s" % __version__)
 Author is Lasha Gogua, Package Version is 0.1.0
 """
@@ -14,8 +17,8 @@ Author is Lasha Gogua, Package Version is 0.1.0
 import genuuid as _id
 from genuuid import __author__,__version__
 
-uuid = _id.suuid()
-uuid1 = _id.luuid() 
+uuid = _id.luuid()
+uuid1 =  _id.suuid()
 print("return uuid is '%s'. " % uuid, "uuid length is %s" % len(uuid))
-print("return uuid is '%s'. " % uuid1, "uuid length is %s" % len(uuid1))
+print("return uuid1 is '%s'. " % uuid1, "uuid1 length is %s" % len(uuid1))
 print("Author is %s," % __author__, "Package Version is %s" % __version__)
